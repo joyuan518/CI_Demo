c1122284c687d14fe1e2332746100e03bbffc7a7
John Yuan <dongyuan518@outlook.com>
2016-07-19 14:55:46 +0800

    Add project files.

---
+++ CI_Demo.Function/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI_Demo.Function
+{
+    public class Calculator
+    {
+        public float Add(float operatorA, float operatorB)
+        {
+            return operatorA + operatorB;
+        }
+
+        public float Sub(float operatorA, float operatorB)
+        {
+            return operatorA - operatorB;
+        }
+
+        public float Multiply(float operatorA, float operatorB)
+        {
+            return operatorA * operatorB;
+        }
+
+        public float Divide(float operatorA, float operatorB)
+        {
+            return operatorA / operatorB;
+        }
+    }
+}
---
+++ CI_Demo.UnitTests/CalculatorTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using CI_Demo.Function;
+
+namespace CI_Demo.UnitTests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void CanAdd()
+        {
+            Assert.AreEqual(3, new Calculator().Add(1, 2));
+        }
+
+        [Test]
+        public void CanSub()
+        {
+            Assert.AreEqual(3, new Calculator().Sub(6, 3));
+        }
+
+        [Test]
+        public void CanMultiply()
+        {
+            Assert.AreEqual(9, new Calculator().Multiply(3, 3));
+        }
+
+        [Test]
+        public void CanDivide()
+        {
+            Assert.AreEqual(3, new Calculator().Divide(9, 3));
+        }
+    }
+}
