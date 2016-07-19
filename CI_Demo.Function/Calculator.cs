dc5ffe047bf98e4a5e38719a2f2192281fdc8e06
John Yuan <dongyuan518@outlook.com>
2016-07-19 18:31:35 +0800

    Remove unused "using" statements.

--- CI_Demo.Function/Calculator.cs
+++ CI_Demo.Function/Calculator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CI_Demo.Function
+﻿namespace CI_Demo.Function
 {
     public class Calculator
     {
